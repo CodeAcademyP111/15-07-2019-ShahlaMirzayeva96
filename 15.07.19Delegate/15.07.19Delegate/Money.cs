fb607fd7a25f8f01b27f33ecfa939ea0e19983ad
ShahlaMirzayeva96 <49427424+ShahlaMirzayeva96@users.noreply.github.com>
2019-07-15 23:48:45 +0400

    Add files via upload

---
+++ 15.07.19Delegate/15.07.19Delegate/Money.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _15._07._19Delegate
+{
+     class Money
+    {
+        public event Action<int,int> GetMoneyEvent;
+        public int Pul { get; private set; }
+         public Money(int n)
+        {
+            Pul = n;
+        }
+
+        public  void GetMoney(int n)
+        {
+            if (n < Pul)
+            {
+                Console.WriteLine($"{n} Manat");
+            }
+            else
+            {
+                GetMoneyEvent.Invoke(Pul,n);
+            }
+        }
+    }
+}
---
+++ 15.07.19Delegate/15.07.19Delegate/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _15._07._19Delegate
+{
+    class Program
+    {
+        //public delegate int DelegateMeth(int m);
+        static void Main(string[] args)
+        {
+
+            //DelegateMeth deleMethod= SimpleNumber;
+            //deleMethod += MinusNumber;
+            //deleMethod += DoubleSingleNum;
+            //deleMethod(18);
+
+            //Func<int,int> delegateMethod = SimpleNumber;
+            //delegateMethod += MinusNumber;
+            //delegateMethod += DoubleSingleNum;
+            //delegateMethod(6);
+
+            Money money = new Money(60);
+            money.GetMoneyEvent += delegate (int x,int y)
+              {
+                  Console.WriteLine($"Kifayet qeder balansiniz yoxdur");
+                  
+              };
+               money.GetMoney(70);
+           
+        }
+        
+        static int SimpleNumber(int n)
+        {
+
+            int count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    count++;
+                }
+               
+
+            }
+            if (count > 2)
+            {
+                Console.WriteLine($"{ n } Murekkeb ededdir");
+
+            }
+            else
+            {
+                
+                Console.WriteLine($"{ n } Sade ededdir");
+
+            }
+            return n;
+           
+        }
+
+        static int MinusNumber(int m)
+        {
+            if (m > 0)
+            {
+                Console.WriteLine($"{m} Musbet ededdir");
+            }
+            else
+            {
+                Console.WriteLine($"{m} Menfi ededdir");
+            }
+            return m;
+        }
+
+        static int DoubleSingleNum(int x)
+        {
+            if (x % 2 == 1)
+            {
+                Console.WriteLine($"{x} Tek ededdir");
+            }else if (x % 2 == 0)
+            {
+
+                Console.WriteLine($"{x} Cut ededdir");
+            }
+            return x;
+        }
+    }
+}
